338d77bcab9d1bc685ff6db112b904259b9ec596
Brandon Liu <luoguliu@gmail.com>
2025-08-15 14:05:39 +0930

    Update run_ner.py

--- run_ner.py
+++ run_ner.py
@@ -3,18 +3,32 @@ import spacy
 # Load a pre-trained spaCy model
 nlp = spacy.load("en_core_web_sm")
 
-# Example text
-f = open('chapter13')
-text = f.read()
 
-# Process the text with spaCy
-doc = nlp(text)
 
-people = set()
+people = dict()
 
-# Iterate through the entities and print person names
-for ent in doc.ents:
-    if ent.label_ == "PERSON":
-        people.add(ent.text)
+for i in range(1, 14):
+    fname = "chapter%02d" % i
+    f = open(fname)
+    text = f.read()
+    f.close()
 
-print(people)
+    # Process the text with spaCy
+    doc = nlp(text)
+
+    # Iterate through the entities and print person names
+    for ent in doc.ents:
+        if ent.label_ == "PERSON":
+            if ent.text in people:
+                people[ent.text] += 1
+            else:
+                people[ent.text] = 1
+
+
+f = open('ner_result', 'w')
+f.write(str(sorted(list(people.items()), key=lambda item: item[1], reverse=True)))
+f.close()
+
+
+
+
